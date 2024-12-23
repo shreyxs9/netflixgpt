0076e179c87c0596d8e925d97d5e3978b7f6032b
Shreyas <106990933+shreyxs9@users.noreply.github.com>
2024-12-23 20:29:59 +0530

    added redux and signout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
+import { auth } from "../utils/firebase";
+import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
-    return (
-        <div className="fixed top-0 left-0 w-44 z-20 p-4">
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      navigate('/');
+    }).catch((error) => {
+      // An error happened.
+      console.error("Sign-out error:", error);
+    });
+  };
+
+  return (
+    <div className='flex w-screen' style={{ background: 'linear-gradient(180deg, rgba(0, 0, 0, 0.8000) 0.000%, rgba(0, 0, 0, 0.7889) 8.333%, rgba(0, 0, 0, 0.7556) 16.67%, rgba(0, 0, 0, 0.7000) 25.00%, rgba(0, 0, 0, 0.6222) 33.33%, rgba(0, 0, 0, 0.5222) 41.67%, rgba(0, 0, 0, 0.4000) 50.00%, rgba(0, 0, 0, 0.2778) 58.33%, rgba(0, 0, 0, 0.1778) 66.67%, rgba(0, 0, 0, 0.1000) 75.00%, rgba(0, 0, 0, 0.04444) 83.33%, rgba(0, 0, 0, 0.01111) 91.67%, rgba(0, 0, 0, 0.000) 100.0%)' }}>
+      <div className="fixed flex top-0 left-0 w-full z-20 p-4 justify-between items-center">
+        <img 
+          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" 
+          alt="Netflix Logo" 
+          className="w-44 h-auto"
+        />
+        {user && (
+          <div className="flex items-center">
             <img 
-                src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" 
-                alt="Netflix Logo" 
-                className="w-full h-auto"
+              src={user.photoURL} // Ensure this is photoURL
+              alt="User Avatar" 
+              className="w-10 h-auto rounded-full"
             />
-        </div>
-    );
+            <button onClick={handleSignOut} className="bg-red-500 text-white font-bold py-2 px-4 ml-4 rounded hover:bg-red-700">
+              Sign Out
+             </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,41 +1,55 @@
 import { useRef, useState } from "react";
 import Header from "./Header";
 import { validate } from "../utils/validate";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from 'react-router-dom'
+import { addUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux";
 
 
 const Login = () => {
-    const [isSignIn, setisSignIn] = useState(false);
-    const [errormessage, setErrormessage] = useState(null);
-    const email = useRef(null);
-    const password = useRef(null);
+    const [isSignUp, setIsSignUp] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const message = validate(email.current.value, password.current.value);
-        setErrormessage(message);
-        if(message) return;
+    const nameRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-        if(!isSignIn){
-            createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-              // Signed up 
-              const user = userCredential.user;
-              // ...
-            })
-            .catch((error) => {
-              const errorCode = error.code;
-              const errorMessage = error.message;
-              // ..
-            });
-        } else {
+    const handleSubmit = async () => { 
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
 
+        const message = validate(email, password);
+        setErrorMessage(message);
+        if (message) return;
+ 
+        try {
+            if (isSignUp) {
+                const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+                console.log("User signed up:", userCredential.user);
+                await updateProfile(userCredential.user, {
+                    displayName: nameRef.current.value,
+                    photoURL: "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                });
+                const {uid,email,displayName,photoURL} = auth.currentUser;
+                dispatch(addUser({uid:uid, email:email, displayName:displayName, photoURL:photoURL}));
+                navigate("/browse");
+            } else {
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+                console.log("User signed in:", userCredential.user);
+                navigate("/browse");
+
+            }
+        } catch (error) {
+            setErrorMessage(`Error: ${error.message}`);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
         }
-    };
+    };                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
-    const toggleSignIn = () => {
-        setisSignIn(!isSignIn);
+    const toggleSignUp = () => {
+        setIsSignUp(!isSignUp);
     };
 
     return (
@@ -44,39 +58,60 @@ const Login = () => {
             <div className="absolute inset-0 z-0">
                 <img
                     src="https://assets.nflxext.com/ffe/siteui/vlv3/151f3e1e-b2c9-4626-afcd-6b39d0b2694f/web/IN-en-20241028-TRIFECTA-perspective_bce9a321-39cb-4cce-8ba6-02dab4c72e53_large.jpg"
-                    alt="bg"
+                    alt="Background"
                     className="w-full h-full object-cover"
                 />
             </div>
-            <form onSubmit={handleSubmit} className="relative z-10 w-3/12 bg-stone-900 bg-opacity-80 text-white p-12 py-16 rounded-lg flex flex-col items-center">
-                <h2 className="text-3xl px-4 pb-4">{isSignIn ? "Sign Up" : "Sign In"}</h2>
-                {isSignIn && (
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit();
+                }}
+                className="relative z-10 w-3/12 bg-stone-900 bg-opacity-80 text-white p-12 py-16 rounded-lg flex flex-col items-center"
+            >
+                <h2 className="text-3xl px-4 pb-4">{isSignUp ? "Sign Up" : "Sign In"}</h2>
+                {isSignUp && (
+                    <label htmlFor="name" className="w-full text-left text-gray-400">
+                        Full Name
+                        <input
+                            id="name"
+                            type="text"
+                            ref={nameRef}
+                            placeholder="Full name"
+                            className="w-full p-3 py-4 m-2 my-4 bg-gray-700 rounded-md outline-none"
+                        />
+                    </label>
+                )}
+                <label htmlFor="email" className="w-full text-left text-gray-400">
+                    Email
                     <input
-                        type="text"
-                        placeholder="Full name"
+                        id="email"
+                        type="email"
+                        ref={emailRef}
+                        placeholder="Email"
                         className="w-full p-3 py-4 m-2 my-4 bg-gray-700 rounded-md outline-none"
                     />
-                )}
-                <input
-                    type="text"
-                    ref={email}
-                    placeholder="Email"
-                    className="w-full p-3 py-4 m-2 my-4 bg-gray-700 rounded-md outline-none"
-                />
-                <input
-                    type="password"
-                    ref={password}
-                    placeholder="Password"
-                    className="w-full p-3 py-4 m-2 my-4 bg-gray-700 rounded-md outline-none"
-                />
-                <p className="p-2 text-red-500 text-left">{errormessage}</p>
+                </label>
+                <label htmlFor="password" className="w-full text-left text-gray-400">
+                    Password
+                    <input
+                        id="password"
+                        type="password"
+                        ref={passwordRef}
+                        placeholder="Password"
+                        className="w-full p-3 py-4 m-2 my-4 bg-gray-700 rounded-md outline-none"
+                    />
+                </label>
+                <p className="p-2 text-red-500 text-left">{errorMessage}</p>
                 <button type="submit" className="w-full p-2 m-2 py-4 my-2 mb-4 bg-red-600 rounded-md hover:bg-red-700">
-                    {isSignIn ? "Sign Up" : "Sign In"}
+                    {isSignUp ? "Sign Up" : "Sign In"}
                 </button>
                 <div className="flex pt-2 justify-start">
-                    <span className="text-gray-400">{!isSignIn ? "New to NetflixGPT?" : "Already a Member?"} </span>
-                    <span onClick={toggleSignIn} className="ml-1 text-white cursor-pointer hover:underline">
-                        {!isSignIn ? "Sign up now." : "Sign In now"}
+                    <span className="text-gray-400">
+                        {isSignUp ? "Already a member?" : "New to NetflixGPT?"}
+                    </span>
+                    <span onClick={toggleSignUp} className="ml-1 text-white cursor-pointer hover:underline">
+                        {isSignUp ? "Sign in now." : "Sign up now."}
                     </span>
                 </div>
             </form>
